9224151ad6b072e404aa5893da569516e45caf4e
Shayne van Asperen <shaynevanasperen@gmail.com>
2015-10-08 15:50:08 +0100

    Remove "public" access modifier from test class

--- Quarks.Tests/PluralizeForCountTests.cs
+++ Quarks.Tests/PluralizeForCountTests.cs
@@ -3,7 +3,7 @@
 namespace Quarks.Tests
 {
 	[Subject(typeof(Inflector))]
-	public class When_using_pluralize_for_count
+	class When_using_pluralize_for_count
 	{
 		It should_pluralize_when_count_is_greater_than_two = () =>
 			"item".PluralizeForCount(2).ShouldEqual("items");
