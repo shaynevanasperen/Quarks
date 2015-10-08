bf173d5e745405d32e2c4fad9eb8bc7e1e034744
Shayne van Asperen <shaynevanasperen@gmail.com>
2015-10-08 15:52:58 +0100

    Added SystemTimeOffset Quark

---
+++ Quarks/SystemTimeOffset.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Quarks
+{
+	static class SystemTimeOffset
+	{
+		static Func<DateTimeOffset> _now;
+		static Func<DateTimeOffset> _utcNow;
+
+		static SystemTimeOffset()
+		{
+			Reset();
+		}
+
+		internal static DateTimeOffset Now
+		{
+			get { return _now(); }
+			set { _now = () => value; }
+		}
+
+		internal static DateTimeOffset UtcNow
+		{
+			get { return _utcNow(); }
+			set { _utcNow = () => value; }
+		}
+
+		internal static void Reset()
+		{
+			_now = () => DateTimeOffset.Now;
+			_utcNow = () => DateTimeOffset.UtcNow;
+		}
+	}
+}
