0ca52620a170306123bdf0497b04f8c0632c01ca
Shayne van Asperen <shaynevanasperen@gmail.com>
2015-11-19 14:55:36 +0000

    Allow packaging only recently modified Quarks

--- Quarks/.Tasks.ps1
+++ Quarks/.Tasks.ps1
@@ -1,10 +1,20 @@
 Include-PluginScripts
 
-task SetVersion
-
-task Pack {
+function Pack-Nuspecs ($dateModified) {
 	New-Item $artifactsPath -Type directory -Force | Out-Null
-	Get-ChildItem $basePath\$projectName\*.nuspec -Recurse | %{ exec { & NuGet pack $_.FullName -OutputDirectory $artifactsPath } }
+	$nuspecs = Get-ChildItem $basePath\$projectName\*.nuspec -Recurse
+	if ($dateModified) {
+		$nuspecs = $nuspecs | Where { $_.LastWriteTime -gt $dateModified }
+	}
+	$nuspecs | %{ exec { & NuGet pack $_.FullName -OutputDirectory $artifactsPath } }
 }
 
-task . Clean, Compile, Test, Pack
+task Pack {
+	Pack-Nuspecs
+}
+
+task PackRecentlyModified {
+	Pack-Nuspecs (Get-Date).AddDays(-1)
+}
+
+task . Clean, Compile, Test, PackRecentlyModified
