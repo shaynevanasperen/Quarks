52c0e9b60c46683974d60b69ac4ffbfa2da7cb73
Shayne van Asperen <shaynevanasperen@gmail.com>
2016-01-18 17:27:52 +0000

    Uninstall old NHibernate.Sessions package and update PowerTasks to latest version

--- Quarks/.Tasks.ps1
+++ Quarks/.Tasks.ps1
@@ -17,4 +17,4 @@ task PackRecentlyModified {
 	Pack-Nuspecs (Get-Date).AddDays(-1)
 }
 
-task . Clean, Compile, Test, PackRecentlyModified, Push
+task . Clean, Compile, MSpec, PackRecentlyModified, Push
--- pt.bat
+++ pt.bat
@@ -10,7 +10,7 @@ if (Test-Path '%LocalAppData%\NuGet\NuGet.exe') { ^
 } ^
 Write-Host 'Restoring NuGet packages'; ^
 NuGet restore; ^
-. '.\packages\PowerTasks.2.0.0\Functions.ps1'; ^
+. '.\packages\PowerTasks.3.0.0\Functions.ps1'; ^
 $basePath = Resolve-Path .; ^
 Set-Location '.\Quarks'; ^
 $basePath = Split-Path (Split-Path (Resolve-Path $basePath -Relative)); ^
