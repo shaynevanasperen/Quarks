0d17ce5f3d4f6c74bed24cb6772d60828e277d7b
Shayne van Asperen <shaynevanasperen@gmail.com>
2016-05-19 10:32:27 +0100

    Update PowerTasks to latest version so that it uses latest NuGet version

--- pt.bat
+++ pt.bat
@@ -3,14 +3,14 @@ powershell -NoProfile -ExecutionPolicy Unrestricted -Command ^
 $ErrorActionPreference = 'Stop'; ^
 if (!(Get-Command NuGet -ErrorAction SilentlyContinue) -and !(Test-Path '%LocalAppData%\NuGet\NuGet.exe')) { ^
 	Write-Host 'Downloading NuGet.exe'; ^
-	(New-Object system.net.WebClient).DownloadFile('https://www.nuget.org/nuget.exe', '%LocalAppData%\NuGet\NuGet.exe'); ^
+	(New-Object system.net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '%LocalAppData%\NuGet\NuGet.exe'); ^
 } ^
 if (Test-Path '%LocalAppData%\NuGet\NuGet.exe') { ^
 	Set-Alias NuGet (Resolve-Path %LocalAppData%\NuGet\NuGet.exe); ^
 } ^
 Write-Host 'Restoring NuGet packages'; ^
 NuGet restore; ^
-. '.\packages\PowerTasks.3.0.0\Functions.ps1'; ^
+. '.\packages\PowerTasks.3.1.0\Functions.ps1'; ^
 $basePath = Resolve-Path .; ^
 Set-Location '.\Quarks'; ^
 $basePath = Split-Path (Split-Path (Resolve-Path $basePath -Relative)); ^
