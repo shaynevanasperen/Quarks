a1c7a6d3d7d702f87a2fb91a1d5e916fb2151d27
yinyue200 <yinyue200@outlook.com>
2017-11-03 20:08:44 +0800

    add a new Contains method

--- Quarks/StringExtensions/Contains.cs
+++ Quarks/StringExtensions/Contains.cs
@@ -9,7 +9,18 @@ namespace Quarks.StringExtensions
 			if (caseSensitive)
 				return input.Contains(value);
 			
-			return input.IndexOf(value, StringComparison.CurrentCultureIgnoreCase) != -1;
+			return input.IndexOf(value, StringComparison.CurrentCultureIgnoreCase) >= 0;
 		}
-	}
+        internal static bool Contains(this string str, string substring, StringComparison comp)
+        {
+            //copy from https://docs.microsoft.com/zh-cn/dotnet/api/system.string.contains?view=netframework-4.7.1
+            if (substring == null)
+                throw new ArgumentNullException(nameof(substring));
+            else if (!Enum.IsDefined(typeof(StringComparison), comp))
+                throw new ArgumentException("comp is not a member of StringComparison",
+                                            nameof(comp));
+
+            return str.IndexOf(substring, comp) >= 0;
+        }
+    }
 }
